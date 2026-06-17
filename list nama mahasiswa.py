a013803469ceb6964aa0cb85c6e20ab09fea7167
saktipurnama900-alprog <saktipurnama900@gmail.com>
2026-06-17 12:12:32 +0800

    Add files via upload

---
+++ list nama mahasiswa.py
@@ -0,0 +1,29 @@
+# Penyimpanan data menggunakan LIST (sesuai contoh struktur data)
+data_mahasiswa = [
+  ["budi sentosa",75],
+  ["asep udin",85],
+  ["sumanto",75],
+  ["saepudin",85],
+]
+
+while True:
+    # Tampilan Menu
+    print("====================================")
+    print(" APLIKASI MANAJEMEN NILAI MAHASISWA")
+    print("====================================")
+    print("1. Tampilkan Data")
+    print("2. Tambah Data")
+    print("3. Ubah Data")
+    print("4. Hapus Data")
+    print("5. Cari Data")
+    print("6. Urutkan Data Berdasarkan Nilai")
+    print("7. Hitung Rata-rata Nilai")
+    print("8. Keluar")
+    print()
+
+    pilihan = input("Pilih menu 1-8: ")
+    print()
+
+  
+
+    
