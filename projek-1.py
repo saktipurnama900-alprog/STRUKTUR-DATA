f7450c9a6090373b6203780dc9a5eaac37d1e664
saktipurnama900-alprog <saktipurnama900@gmail.com>
2026-06-17 12:15:32 +0800

    Add files via upload

---
+++ projek-1.py
@@ -0,0 +1,17 @@
+nama = input("masukan nama anda:")
+
+if nama=="danu saputra":
+    print("selamat datang ahmad yani")
+    
+umur = input("masukan umur anda")
+
+if umur==20:
+    print("anda cukup umur:")
+elif umur==0:
+    print("anda belum lahir:")
+elif umur==17:
+    print("anda belum cukup umur:")
+elif umur==105:
+    print("anda sudah bau tanah.perbanyak ibadah")
+    
+print("program selesai")
